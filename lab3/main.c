c49a5d3b734f5dc5d0f9e737c337f3597b9a4918
KiRiLL625 <70346582+KiRiLL625@users.noreply.github.com>
2024-10-17 22:40:33 +0300

    Create main.c

---
+++ lab3/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+void atexit_func(){
+    printf("[ATEXIT] Atexit process, id: %d\n", getpid());
+}
+
+void handler(int signum){
+    switch(signum){
+        case SIGINT:
+            printf("[SIGNAL] SIGINT signal received, pid: %d\n", getpid());
+            break;
+        case SIGTERM:
+            printf("[SIGNAL] SIGTERM signal received, pid: %d\n", getpid());
+            break;
+        default:
+            printf("[SIGNAL] Signal %d received, pid: %d\n", signum, getpid());
+            break;
+    }
+}
+
+int main(void) {
+    if(signal(SIGINT, handler) == SIG_ERR){
+        printf("Cannot catch SIGINT\n");
+    }
+    struct sigaction sa;
+    sa.sa_handler = handler;
+    sa.sa_flags = SA_SIGINFO;
+
+    if(sigaction(SIGTERM, &sa, NULL) == -1){
+        printf("Cannot catch SIGTERM\n");
+    }
+
+    if(atexit(atexit_func) != 0){
+        perror("Atexit failed\n");
+    }
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        int err = errno;
+        fprintf(stderr, "Fork failed: %s\n", strerror(err));
+        return 1;
+    } else if (pid == 0) {
+        printf("[CHILD] This is the child process, my pid: %d, parent pid: %d\n", getpid(), getppid());
+    } else {
+        int status;
+        sleep(7);
+        wait(&status);
+        printf("[PARENT] This is the parent process, pid: %d, child pid is %d, parent pid is %d\n", getpid(), pid, getppid());
+        printf("[PARENT} Child process exited with status %d\n", WEXITSTATUS(status));
+    }
+
+    return 0;
+}
