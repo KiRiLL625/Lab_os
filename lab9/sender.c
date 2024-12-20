0bb14ebcdc8c68873522280881d1e9938cc5f576
KiRiLL625 <70346582+KiRiLL625@users.noreply.github.com>
2024-12-20 12:12:49 +0300

    Update sender.c

--- lab9/sender.c
+++ lab9/sender.c
@@ -3,65 +3,86 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
 #include <sys/stat.h>
 #include <time.h>
 #include <string.h>
 #include <signal.h>
-#include <sys/sem.h>
 
-#define SHM_NAME "/shmemory"
+#define FTOK_PATH "."
 #define MAX_LEN 256
-#define SEM_KEY 52
 
 typedef struct {
     pid_t pid;
     char time_str[MAX_LEN];
+    //int data_ready;
 } shared_data;
 
-// Обработка сигнала Ctrl+C
+int shmid;
+int semid;
+
+shared_data *shm_ptr;
+
+//Ctrl+C
 void handle_sigint(int sig) {
-    shm_unlink(SHM_NAME);
-    perror("Получен SIGINT, выход");
+    shmdt(shm_ptr);
+    if(shmctl(shmid, IPC_RMID, NULL)){
+        perror("shmctl");
+    }
+    else{
+        printf("SIGINT received, shmem deleted\n");
+    }
+
+    if(semctl(semid, 0, IPC_RMID)){
+        perror("semctl");
+    }
+    else{
+        printf("Semaphore deleted\n");
+    }
     exit(0);
 }
 
 int main() {
-    int shm_fd;
-    shared_data *shm_ptr;
-    int sem_id;
-    struct sembuf sem_op;
+    key_t key;
 
     signal(SIGINT, handle_sigint);
 
-    // Создание семафора
-    sem_id = semget(SEM_KEY, 1, IPC_CREAT | 0666);
-    if (sem_id == -1) {
+    //Проверка существования разделяемой памяти
+    key = ftok(FTOK_PATH, 'A');
+    if (key == -1) {
+        perror("ftok");
+        exit(1);
+    }
+
+    semid = semget(key, 1, IPC_CREAT | IPC_EXCL | 0666);
+    if(semid == -1){
         perror("semget");
+        printf("Sender already running\n");
         exit(1);
     }
 
-    // Инициализация семафора значением 0
-    if (semctl(sem_id, 0, SETVAL, 0) == -1) {
-        perror("semctl");
+    if (semctl(semid, 0, SETVAL, 1) == -1) {
+        perror("semctl_setval");
+        semctl(semid, 0, IPC_RMID);
         exit(1);
     }
 
-    // Создание разделяемой памяти
-    shm_fd = shm_open(SHM_NAME, O_CREAT | O_EXCL | O_RDWR, 0666);
-    if (shm_fd == -1) {
-        perror("Shared memory already exists");
+    shmid = shmget(key, MAX_LEN, IPC_CREAT | IPC_EXCL | 0666);
+    if(shmid == -1){
+        perror("shmget");
+        printf("Shared memory already exists\n");
         exit(1);
     }
 
-    // Установка размера разделяемой памяти
-    ftruncate(shm_fd, sizeof(shared_data));
-
-    // Отображение разделяемой памяти в адресное пространство
-    shm_ptr = mmap(0, sizeof(shared_data), PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    if (shm_ptr == MAP_FAILED) {
-        perror("mmap");
-        exit(1);
+    shm_ptr = (shared_data*) shmat(shmid, NULL, 0);
+    if(shm_ptr == (void*) -1){
+        perror("shmat");
+        shmctl(shmid, IPC_RMID, NULL);
+        semctl(semid, 0, IPC_RMID);
     }
+
+    struct sembuf sem = {0, 0, 0};
 
     while (1) {
         time_t now = time(NULL);
@@ -71,15 +92,12 @@ int main() {
 
         shm_ptr->pid = getpid();
         strncpy(shm_ptr->time_str, time_str, MAX_LEN);
-
-        // Сигнализация receiver
-        sem_op.sem_num = 0;
-        sem_op.sem_op = 1; // Увеличение семафора
-        sem_op.sem_flg = 0;
-        if (semop(sem_id, &sem_op, 1) == -1) {
+        sem.sem_op = 1;
+        if (semop(semid, &sem, 1) == -1) {
             perror("semop");
             exit(1);
         }
+
         sleep(1);
     }
 
